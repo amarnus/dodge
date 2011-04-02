30c714f4e0f06e4454f4d30d57c099444f6ea75b
amarnus <amarnus@gmail.com>
2011-04-02 12:39:13 +0800

    Removed files

--- static/boot.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  $(document).ready(function() {
-    $("body").load("https://github.com/amarnus/dodge/raw/master/index.html", function(content) {
-      console.log(content);
-    });
-  });
-})(jQuery);
